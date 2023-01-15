3ce43cb65525ce9bcdef4a739c1e6c5194c073d7
FRuktozza <121963443+FRuktozza@users.noreply.github.com>
2023-01-15 11:09:25 +0300

    Add files via upload

---
+++ Task19/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (IsValidNumber(num))
+        {
+            if (IsPalindrome(num))
+            {
+                Console.WriteLine("да");
+            }
+            else
+            {
+                Console.WriteLine("нет");
+            }
+        }
+        else
+        {
+            Console.WriteLine("некорректный ввод");
+        }
+    }
+
+    static bool IsPalindrome(int num) 
+    {
+        return (num / 10000 == num % 10) && (num / 1000 % 10 == num / 10 % 10);
+    }
+
+    static bool IsValidNumber(int num)
+    {
+        return (num >= 10000) && (num < 100000);
+    }
+}
---
+++ Task21/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите координаты точек");
+
+        int[] coords = new int[6];
+
+        for (int i = 0; i < coords.Length; i++)
+        {
+            coords[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        double result = Distance(coords);
+        Console.WriteLine(Math.Round(result, 2));
+    }
+
+    static double Distance(int[] coords)
+    {
+        double res = 0.0;
+        for (int i = 0; i < coords.Length / 2; i++)
+        {
+            res += Math.Pow((coords[i] - coords[i + 3]), 2);
+        }
+
+        return Math.Sqrt(res);
+    }
+}
